ca288a1e15c5ed8aa49650e96d9a932b45afc6f5
nadimk1 <33365895+nadimk1@users.noreply.github.com>
2020-10-20 01:45:01 -0400

    updated design of the map and of the navigation bar

--- Park-Me/src/Navigation.js
+++ Park-Me/src/Navigation.js
@@ -1,48 +1,59 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import Logo from "./Parking-Logo.svg";
 
 class Navigation extends Component {
+  render() {
+    const { user, logOutUser } = this.props;
 
-
-render(){
-
-  const {user, logOutUser} = this.props;
-
-        return(
-<nav className="site-nav family-sans navbar navbar-expand bg-primary navbar-dark higher">
-<style>
-            {
-              "\
-        .bg-primary{\
-          background-color: black;\
+    return (
+      <nav className="site-nav family-sans navbar navbar-expand navbar-dark higher">
+        <style>
+          {
+            "\
+        .navbar{\
+          background-color: Black;\
         }\
       "
-  
-            }
-          </style>
-  <div className="container-fluid">
-    <a href="/" className="navbar-brand">
-      Park Me
-    </a>
-    <div className="navbar-nav ml-auto">
-    <a className="nav-item nav-link" href="/login">
-          {this.props.user}
-        </a>
-        {user == null && (<a className="nav-item nav-link" href="/login">
-          Log in
-        </a>)}
+          }
+        </style>
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img
+              src={Logo}
+              width="30"
+              height="30"
+            />
+          </a>
 
-        {user == null &&(<a className="nav-item nav-link" href="/register">
-          Register
-        </a>
-        )}
-        {user &&(<a className="nav-item nav-link" href="/login" onClick ={e => logOutUser(e)}>
-          Log out
-        </a>)}
-    </div>
-  </div>
-</nav>
-        )
-    }
+          <div className="navbar-nav ml-auto">
+            <a className="nav-item nav-link" href="/login">
+              {this.props.user}
+            </a>
+            {user == null && (
+              <a className="nav-item nav-link" href="/login">
+                Log in
+              </a>
+            )}
+
+            {user == null && (
+              <a className="nav-item nav-link" href="/register">
+                Register
+              </a>
+            )}
+            {user && (
+              <a
+                className="nav-item nav-link"
+                href="/login"
+                onClick={(e) => logOutUser(e)}
+              >
+                Log out
+              </a>
+            )}
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Navigation;
