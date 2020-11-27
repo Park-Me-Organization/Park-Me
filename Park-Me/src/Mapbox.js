03deb531dd8304564f211354906bebf010a26f31
nadimk1 <33365895+nadimk1@users.noreply.github.com>
2020-11-27 00:35:40 -0500

    update to the userInput page

--- Park-Me/src/Mapbox.js
+++ Park-Me/src/Mapbox.js
@@ -4,7 +4,7 @@ import mapboxgl from "mapbox-gl";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import "./App.css";
 import * as parkingdata from "./parking.geojson";
-import getResults from './MapboxAJAX';
+import getResults from "./MapboxAJAX";
 import UserInput from "./UserInput";
 
 var geocoderGlobal = "";
@@ -38,7 +38,7 @@ class Mapbox extends Component {
   componentDidMount() {
     mapboxgl.accessToken =
       "pk.eyJ1IjoibmFkaW1rMSIsImEiOiJja2doaGh5dWowM292MnpudW03MHc2MzdwIn0.TU9JkM8-F3FZ5RKTTO3n9A";
-    
+
     //Mapbox Map View
     const map = new mapboxgl.Map({
       container: this.mapContainer,
@@ -56,14 +56,8 @@ class Mapbox extends Component {
       mapboxgl: mapboxgl, // Set the mapbox-gl instance
       placeholder: "Address, Place, City or Venue",
       marker: false, // Do not use the default marker style
-      limit: 10,  //limits the search suggestion results
-      types: 'poi',
-      filter: function (item) {
-        // returns true if item contains New South Wales region
-        if (item.properties.category === "parking, parking lot") {
-        return item.context
-        }   
-    },
+      limit: 10, //limits the search suggestion results
+      types: "poi",
       //render geocoder for suggested list of items with logo,text and address
       render: function (item) {
         var maki = item.properties.maki || "marker";
@@ -112,82 +106,78 @@ class Mapbox extends Component {
           features: [],
         },
       });
-  
 
-    //add single point mark for geocoder result
-    map.addLayer({
-      id: 'point',
-      source: 'single-point',
-      type: 'symbol',
-      "layout": {
-        "icon-image": "parking-15",
-        "icon-allow-overlap": true,
-      }
-    }
-    );
-  
-    //add parking spots from json file
-    map.addLayer({
-      id: "locations",
-      type: "symbol",
-      /* Add a GeoJSON source containing place coordinates and information. */
-      source: {
-        type: "geojson",
-        data: parkingdata,
-      },
-      layout: {
-        "icon-image": "parking-15",
-        "icon-allow-overlap": true,
-      },
-    });
-    map.on('click', 'locations', function (e) {
-      var coordinates = e.features[0].geometry.coordinates.slice();
-      var address = e.features[0].properties.address;
-      var place_name = e.features[0].place_name;
-      // Ensure that if the map is zoomed out such that multiple
-      // copies of the feature are visible, the popup appears
-      // over the copy being pointed to.
-      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-      }
-       
-      new mapboxgl.Popup()
-      .setLngLat(coordinates)
-      .setHTML('<h3>'+place_name+'</h3>'+'<p>'+address+'</p>')
-      .addTo(map);
+      //add single point mark for geocoder result
+      map.addLayer({
+        id: "point",
+        source: "single-point",
+        type: "symbol",
+        layout: {
+          "icon-image": "parking-15",
+          "icon-allow-overlap": true,
+        },
+      });
+
+      //add parking spots from json file
+      map.addLayer({
+        id: "locations",
+        type: "symbol",
+        /* Add a GeoJSON source containing place coordinates and information. */
+        source: {
+          type: "geojson",
+          data: parkingdata,
+        },
+        layout: {
+          "icon-image": "parking-15",
+          "icon-allow-overlap": true,
+        },
+      });
+      map.on("click", "locations", function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var address = e.features[0].properties.address;
+        var place_name = e.features[0].place_name;
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setHTML("<h3>" + place_name + "</h3>" + "<p>" + address + "</p>")
+          .addTo(map);
       });
       // Change the cursor to a pointer when the mouse is over the places layer.
-      map.on('mouseenter', 'locations', function () {
-        map.getCanvas().style.cursor = 'pointer';
-        });
-        
-        // Change it back to a pointer when it leaves.
-      map.on('mouseleave', 'locations', function () {
-        map.getCanvas().style.cursor = '';
+      map.on("mouseenter", "locations", function () {
+        map.getCanvas().style.cursor = "pointer";
       });
-    // Listen for the `result` event from the Geocoder
-    // `result` event is triggered when a user makes a selection
-    //  Add a marker at the result's coordinates
-    
-    // geocoder.on('result', function(e) {
-    //   map.getSource('single-point').setData(e.result.geometry);
-      
-    // });
 
-    geocoder.on('result', function(result) {
-      map.getSource('single-point').setData(result.result.geometry);
-    
-      var lat = result.result.center[1];
-      var long = result.result.center[0];
-      
-      getResults(long,lat);
-      console.log("_____\nName\n", result.result.place_name);
-      console.log("Lat/Long", lat, ",", long, "\n_____");
-    });    
-  });
+      // Change it back to a pointer when it leaves.
+      map.on("mouseleave", "locations", function () {
+        map.getCanvas().style.cursor = "";
+      });
+      // Listen for the `result` event from the Geocoder
+      // `result` event is triggered when a user makes a selection
+      //  Add a marker at the result's coordinates
 
-}
+      // geocoder.on('result', function(e) {
+      //   map.getSource('single-point').setData(e.result.geometry);
 
+      // });
+
+      geocoder.on("result", function (result) {
+        map.getSource("single-point").setData(result.result.geometry);
+
+        var lat = result.result.center[1];
+        var long = result.result.center[0];
+
+        getResults(long, lat);
+        console.log("_____\nName\n", result.result.place_name);
+        console.log("Lat/Long", lat, ",", long, "\n_____");
+      });
+    });
+  }
 
   newInput(event) {
     this.setState({
@@ -195,7 +185,6 @@ class Mapbox extends Component {
     });
   }
 
-  
   render() {
     return (
       <div className="container-fluid">
--- Park-Me/src/UserInput.js
+++ Park-Me/src/UserInput.js
@@ -1,31 +1,16 @@
-import React, { useState } from "react";
-// import Slider from "@material-ui/core/Slider";
+import React, { Component, useState } from "react";
 import Slider from "./Slider.js";
 import Button from "react-bootstrap/Button";
-import ReactDOM from "react-dom";
 
-function UserInput(props) {
-  const [currPage, setCurrPage] = useState(["Search"]);
-  const geocoder = props.geocoder;
-  const map = props.map;
-  console.log("GeoCoder: ", geocoder);
-  console.log("map: ", map);
-
-  function changePage() {
-    console.log("changePage | ", currPage);
-    if (currPage == "Search") {
-      setCurrPage("Slider");
-      console.log("after change: ", currPage);
-      console.log(document.getElementById("geocoder"));
-    } else if (currPage == "Slider") {
-      setCurrPage("Search");
-      console.log("after change:", currPage);
-      console.log(document.getElementById("geocoder"));
-    }
+class UserInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: "",
+    };
   }
 
-  //Search bar input | for address
-  function SearchInput(props) {
+  render() {
     return (
       <div>
         <h1 id="Title">Where Do You Want To Go?</h1>
@@ -37,74 +22,134 @@ function UserInput(props) {
             textAlign: "center",
           }}
         >
-          <div id="geocoder"></div>
-        </div>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Address"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+              value={this.state.Address}
+            />
+            <div className="input-group-append">
+              <Button
+                variant="secondary"
+                style={{
+                  backgroundColor: "#1A2637",
+                  fontfamily: "Roboto Slab",
+                }}
+                onClick={console.log(this.state.Address)}
+              >
+                GO
+              </Button>
+            </div>
 
-        <Button
-          variant="secondary"
-          style={{
-            marginTop: "20px",
-            backgroundColor: "#1A2637",
-            width: "100px",
-            padding: "8px",
-            fontfamily: "Roboto Slab",
-          }}
-          onClick={changePage}
-        >
-          GO
-        </Button>
-      </div>
-    );
-  }
-
-  //slider input | for search range
-  function SliderPage(props) {
-    return (
-      <div>
-        <h1 id="Title">What is your radius?</h1>
-        <div
-          style={{
-            marginLeft: "auto",
-            marginRight: "auto",
-            width: "50%",
-            textAlign: "center",
-          }}
-        >
-          <Slider />
-
-          <Button
-            variant="secondary"
-            style={{
-              marginTop: "20px",
-              backgroundColor: "#1A2637",
-              width: "100px",
-              padding: "8px",
-              fontfamily: "Roboto Slab",
-            }}
-            onClick={changePage}
-          >
-            GO
-          </Button>
+            <div id="geocoder"></div>
+          </div>
         </div>
       </div>
     );
   }
-
-  //controls viewable component | conditional components
-  function InputPage(props) {
-    console.log(currPage);
-    if (currPage == "Search") {
-      console.log("search page");
-      return <SearchInput />;
-    }
-    return <SliderPage />;
-  }
-
-  return (
-    <div>
-      <InputPage />
-    </div>
-  );
 }
 
 export default UserInput;
+// function UserInput(props) {
+//   const [currPage, setCurrPage] = useState(["Search"]);
+
+//   function changePage() {
+//     console.log("changePage | ", currPage);
+//     if (currPage == "Search") {
+//       setCurrPage("Slider");
+//       console.log("after change: ", currPage);
+//       console.log(document.getElementById("geocoder"));
+//     } else if (currPage == "Slider") {
+//       setCurrPage("Search");
+//       console.log("after change:", currPage);
+//       console.log(document.getElementById("geocoder"));
+//     }
+//   }
+
+//   //Search bar input | for address
+//   function SearchInput(props) {
+//     return (
+//       <div>
+//         <h1 id="Title">Where Do You Want To Go?</h1>
+//         <div
+//           style={{
+//             marginLeft: "auto",
+//             marginRight: "auto",
+//             width: "55%",
+//             textAlign: "center",
+//           }}
+//         >
+//           <div id="geocoder"></div>
+//         </div>
+
+//         <Button
+//           variant="secondary"
+//           style={{
+//             marginTop: "20px",
+//             backgroundColor: "#1A2637",
+//             width: "100px",
+//             padding: "8px",
+//             fontfamily: "Roboto Slab",
+//           }}
+//           onClick={changePage}
+//         >
+//           GO
+//         </Button>
+//       </div>
+//     );
+//   }
+
+//   //slider input | for search range
+//   function SliderPage(props) {
+//     return (
+//       <div>
+//         <h1 id="Title">What is your radius?</h1>
+//         <div
+//           style={{
+//             marginLeft: "auto",
+//             marginRight: "auto",
+//             width: "50%",
+//             textAlign: "center",
+//           }}
+//         >
+//           <Slider />
+
+//           <Button
+//             variant="secondary"
+//             style={{
+//               marginTop: "20px",
+//               backgroundColor: "#1A2637",
+//               width: "100px",
+//               padding: "8px",
+//               fontfamily: "Roboto Slab",
+//             }}
+//             onClick={changePage}
+//           >
+//             GO
+//           </Button>
+//         </div>
+//       </div>
+//     );
+//   }
+
+//   //controls viewable component | conditional components
+//   function InputPage(props) {
+//     console.log(currPage);
+//     if (currPage == "Search") {
+//       console.log("search page");
+//       return <SearchInput />;
+//     }
+//     return <SliderPage />;
+//   }
+
+//   return (
+//     <div>
+//       <InputPage />
+//     </div>
+//   );
+// }
+
+// export default UserInput;
