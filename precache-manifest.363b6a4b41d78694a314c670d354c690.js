e5d47f94c9c09b18391a1b22562cfd24ebbe61ec
Abu Nadim <nadimkabir2@gmail.com>
2020-11-30 15:19:33 -0500

    Updates

---
+++ precache-manifest.363b6a4b41d78694a314c670d354c690.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47c1314208fdf89d99a2444534c38ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3128447ab61b05540f6",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "e79c895a68be1d238c34",
+    "url": "/static/css/main.30673e87.chunk.css"
+  },
+  {
+    "revision": "d3128447ab61b05540f6",
+    "url": "/static/js/2.e6ebb5c3.chunk.js"
+  },
+  {
+    "revision": "a23cb6af80293164014259a41daa5ef6",
+    "url": "/static/js/2.e6ebb5c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e79c895a68be1d238c34",
+    "url": "/static/js/main.a9757e83.chunk.js"
+  },
+  {
+    "revision": "fac8f58e8f812c1183b6",
+    "url": "/static/js/runtime-main.32caf428.js"
+  },
+  {
+    "revision": "749bf3c66647660091fb24a591168b22",
+    "url": "/static/media/Map_marker.749bf3c6.svg"
+  },
+  {
+    "revision": "5a95957db6079e8fa04a6ced5fe61365",
+    "url": "/static/media/Parking-Logo.5a95957d.svg"
+  },
+  {
+    "revision": "4aadf9fe82bfa49c597998022f1c73a2",
+    "url": "/static/media/parking.4aadf9fe.geojson"
+  }
+]);
